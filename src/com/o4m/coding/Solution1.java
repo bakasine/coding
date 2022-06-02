85a33838be3ee2032e168a1f55933a40994e3942
UerAx <uerax@live.com>
2022-06-03 01:31:01 +0800

    add

--- src/com/o4m/coding/Solution1.java
+++ src/com/o4m/coding/Solution1.java
@@ -1,6 +1,10 @@
 package com.o4m.coding;
 
+import com.o4m.entity.ListNode;
 import com.o4m.entity.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution1 {
 
@@ -43,5 +47,97 @@ public class Solution1 {
             }
         }
         return ans;
+    }
+
+    // lc1 两数之和
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(nums[i])) {
+                return new int[]{i, hash.get(nums[i])};
+            }
+            hash.put(target - nums[i], i);
+        }
+        return null;
+    }
+
+    // lc160 相交链表
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+
+    // lc200 岛屿数量
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    landDfs(grid, dp, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void landDfs(char[][] grid, int[][] dp, int x, int y) {
+        if (x >= grid.length || x < 0 || y >= grid[0].length || y < 0 || dp[x][y] != 0 || grid[x][y] != '1') {
+            return;
+        }
+
+        dp[x][y] = 2;
+
+        landDfs(grid, dp, x+1 , y);
+        landDfs(grid, dp, x , y+1);
+        landDfs(grid, dp, x-1 , y);
+        landDfs(grid, dp, x , y-1);
+
+        dp[x][y] = 1;
+
+
+    }
+
+
+    // lc450 删除二叉搜索树中的节点
+    public TreeNode deleteNode(TreeNode root, int key) {
+        deleteDfs(root, key);
+        return root;
+    }
+
+    public TreeNode leftNode(TreeNode root) {
+        if (root.right != null) {
+            return root.right;
+        }
+        return leftNode(root.left);
+    }
+
+    public void deleteDfs(TreeNode root, int key) {
+        if (root == null) {
+            return;
+        }
+        if (root.val > key) {
+            deleteNode(root.left, key);
+        } else if (root.val < key) {
+            deleteNode(root.right, key);
+        } else {
+            if (root.left == null && root.right == null) {
+                root = null;
+            } else if (root.right != null) {
+                root.val = root.right.val;
+                deleteNode(root.right, root.val);
+            } else {
+                TreeNode left = leftNode(root.left);
+                root.val = left.val;
+                left = null;
+            }
+        }
     }
 }
